fa09fd8518f4ffff1c1c714bac5009d6f6ac445d
Marco Giustini <info@marcogiustini.info>
2025-08-27 19:43:47 +0200

    Update dokan-vendor-notifications.php

--- includes/dokan-vendor-notifications.php
+++ includes/dokan-vendor-notifications.php
@@ -1,40 +1,18 @@
 <?php
 /**
- * Notifiche personalizzate ai vendor Dokan alla chiusura di un ordine collettivo
+ * Invia notifiche personalizzate ai vendor Dokan alla chiusura di un ordine collettivo
  */
 
-//  Raggruppa prodotti per vendor
-function wgo_group_items_by_vendor($wc_order) {
-    $vendor_map = [];
-
-    foreach ($wc_order->get_items() as $item) {
-        $product = $item->get_product();
-        if (!$product) continue;
-
-        $vendor_id = get_post_field('post_author', $product->get_id());
-        if (!isset($vendor_map[$vendor_id])) {
-            $vendor_map[$vendor_id] = [];
-        }
-
-        $vendor_map[$vendor_id][] = [
-            'name' => $product->get_name(),
-            'qty' => $item->get_quantity(),
-            'price' => $item->get_total(),
-        ];
-    }
-
-    return $vendor_map;
-}
-
-//  Invia email personalizzata a ciascun vendor
 function wgo_notify_dokan_vendors($wc_order, $group_id, $pickup_point) {
+    if (!function_exists('wgo_group_items_by_vendor')) return;
+
     $vendor_map = wgo_group_items_by_vendor($wc_order);
 
     foreach ($vendor_map as $vendor_id => $items) {
         $vendor = get_userdata($vendor_id);
         if (!$vendor) continue;
 
-        $vendor_name = $vendor->display_name;
+        $vendor_name  = $vendor->display_name;
         $vendor_email = $vendor->user_email;
 
         $lines = '';
@@ -52,10 +30,5 @@ function wgo_notify_dokan_vendors($wc_order, $group_id, $pickup_point) {
                  . "Totale da ricevere: €" . number_format($total, 2);
 
         wp_mail($vendor_email, 'Ordine collettivo da Gruppo #' . $group_id, $message);
-
-        //  (Facoltativo) trasferimento fondi al wallet del vendor
-        if (function_exists('wgo_transfer_funds')) {
-            wgo_transfer_funds('group', $group_id, 'vendor', $vendor_id, $total);
-        }
     }
 }
