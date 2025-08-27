61c9ddd34ab5d4541e31afa436dd3abd933a497f
Marco Giustini <info@marcogiustini.info>
2025-08-27 16:19:44 +0200

    Update frontend-order-ui.php

--- includes/frontend-order-ui.php
+++ includes/frontend-order-ui.php
@@ -1,3 +1,5 @@
 function wgo_render_order_ui($group_id, $order_id) {
-    // Form con quantità, versamento fondi, nonce, validazioni
+    $pickup = wgo_get_group_pickup_point($group_id);
+    echo '<h4>Punto di Ritiro</h4>';
+    echo '<p>' . esc_html($pickup) . '</p>';
 }
