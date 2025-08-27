f3151c615076369ff997c7c3221e9fc3ec7b5645
Marco Giustini <info@marcogiustini.info>
2025-08-27 19:43:03 +0200

    Update dokan-order-routing.php

--- includes/dokan-order-routing.php
+++ includes/dokan-order-routing.php
@@ -1,4 +1,8 @@
 <?php
+/**
+ * Raggruppa i prodotti di un ordine WooCommerce per vendor Dokan
+ */
+
 function wgo_group_items_by_vendor($wc_order) {
     $vendor_map = [];
 
@@ -6,14 +10,15 @@ function wgo_group_items_by_vendor($wc_order) {
         $product = $item->get_product();
         if (!$product) continue;
 
-        $vendor_id = wgo_get_dokan_vendor_id($product->get_id());
+        $vendor_id = get_post_field('post_author', $product->get_id());
         if (!isset($vendor_map[$vendor_id])) {
             $vendor_map[$vendor_id] = [];
         }
 
         $vendor_map[$vendor_id][] = [
-            'name' => $product->get_name(),
-            'qty' => $item->get_quantity(),
+            'name'  => $product->get_name(),
+            'qty'   => $item->get_quantity(),
+            'price' => $item->get_total(),
         ];
     }
 
