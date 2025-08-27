53583a05dac462e54b3a341051e2faa8ea93b10c
Marco Giustini <info@marcogiustini.info>
2025-08-27 16:23:49 +0200

    Update global-pickup-point.php

--- includes/global-pickup-point.php
+++ includes/global-pickup-point.php
@@ -1,6 +1,1 @@
-function wgo_set_global_pickup_point($address) {
-    update_option('wgo_global_pickup_point', sanitize_text_field($address));
-}
-function wgo_get_global_pickup_point() {
-    return get_option('wgo_global_pickup_point');
-}
+function wgo_set_global_pick
