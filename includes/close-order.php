6cc4488d18f550ba11b1505fddda55e220ae19f1
Marco Giustini <info@marcogiustini.info>
2025-08-27 16:20:14 +0200

    Update close-order.php

--- includes/close-order.php
+++ includes/close-order.php
@@ -1,3 +1,15 @@
 function wgo_close_order($order_id) {
-    // Aggrega quantità, crea ordini WooCommerce, trasferisce fondi, notifica vendor
+    $group_id = get_post_meta($order_id, 'group_id', true);
+    $pickup_point = wgo_get_group_pickup_point($group_id);
+
+    $order = wc_get_order($order_id);
+    $order->update_meta_data('wgo_pickup_point', $pickup_point);
+    $order->save();
+
+    $vendor_id = get_post_field('post_author', $order_id);
+    wp_mail(
+        get_userdata($vendor_id)->user_email,
+        'Nuovo ordine collettivo da Gruppo #' . $group_id,
+        'Hai ricevuto un ordine collettivo. Spedisci la merce al punto di ritiro: ' . $pickup_point
+    );
 }
