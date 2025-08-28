de47b9e6077be1c823812f7d6b1b0ac88def541c
Marco Giustini <info@marcogiustini.info>
2025-08-28 16:40:06 +0200

    Update wgo.php

--- wgo.php
+++ wgo.php
@@ -5,7 +5,7 @@ Description: Gestione ordini per gruppi di acquisto solidali con BuddyPress, Woo
 Version: 1.0.0
 Author: Marco Giustini
 Author URI: https://github.com/marcogiustini
-Text Domain: wp-gas-main
+Text Domain: WP-GAS-main
 Domain Path: /languages
 Requires at least: 6.0
 Tested up to: 6.8
@@ -27,26 +27,14 @@ $plugin_version = $plugin_data['Version'];
 
 // 🎨 Carica CSS e JS con versione esplicita
 add_action('wp_enqueue_scripts', function () use ($plugin_version) {
-    wp_enqueue_style(
-        'wgo-style',
-        plugin_dir_url(__FILE__) . 'assets/css/wgo-style.css',
-        [],
-        $plugin_version
-    );
-
-    wp_enqueue_script(
-        'wgo-script',
-        plugin_dir_url(__FILE__) . 'assets/js/wgo-script.js',
-        [],
-        $plugin_version,
-        true
-    );
+    wp_enqueue_style('wgo-style', plugin_dir_url(__FILE__) . 'assets/css/wgo-style.css', [], $plugin_version);
+    wp_enqueue_script('wgo-script', plugin_dir_url(__FILE__) . 'assets/js/wgo-script.js', [], $plugin_version, true);
 });
 
 // 🔍 Verifica dipendenze minime
 add_action('admin_init', function () {
     if (!function_exists('wc_get_product') || !function_exists('groups_get_group_members')) {
         deactivate_plugins(plugin_basename(__FILE__));
-        wp_die(esc_html__('Questo plugin richiede WooCommerce e BuddyPress attivi.', 'wp-gas-main'));
+        wp_die(esc_html__('Questo plugin richiede WooCommerce e BuddyPress attivi.', 'WP-GAS-main'));
     }
 });
