3b9e3902a3aa899d9d7ddf2213dc4c90db001fed
Marco Giustini <info@marcogiustini.info>
2025-08-28 16:06:09 +0200

    Update wgo-group-pickup-point.php

--- includes/wgo-group-pickup-point.php
+++ includes/wgo-group-pickup-point.php
@@ -6,26 +6,26 @@
 defined('ABSPATH') || exit;
 
 // 🧩 Aggiunge il campo nel backend del gruppo BuddyPress
-function wpgas_render_group_pickup_field($group_id) {
-    $pickup = groups_get_groupmeta($group_id, 'wpgas_group_pickup');
+function wgo_render_group_pickup_field($group_id) {
+    $pickup = groups_get_groupmeta($group_id, 'wgo_group_pickup');
     ?>
-    <label for="wpgas_group_pickup"><?php echo esc_html__('Punto di ritiro del gruppo:', 'wpgas'); ?></label><br>
-    <input type="text" name="wpgas_group_pickup" id="wpgas_group_pickup" value="<?php echo esc_attr($pickup); ?>">
-    <?php wp_nonce_field('wpgas_save_group_pickup', 'wpgas_group_pickup_nonce'); ?>
+    <label for="wgo_group_pickup"><?php echo esc_html__('Punto di ritiro del gruppo:', 'WP-GAS-main'); ?></label><br>
+    <input type="text" name="wgo_group_pickup" id="wgo_group_pickup" value="<?php echo esc_attr($pickup); ?>">
+    <?php wp_nonce_field('wgo_save_group_pickup', 'wgo_group_pickup_nonce'); ?>
     <?php
 }
-add_action('groups_custom_group_fields_editable', 'wpgas_render_group_pickup_field');
+add_action('groups_custom_group_fields_editable', 'wgo_render_group_pickup_field');
 
 // 💾 Salvataggio sicuro del metadato
-function wpgas_save_group_pickup_field($group_id) {
-    $nonce = isset($_POST['wpgas_group_pickup_nonce']) ? sanitize_text_field(wp_unslash($_POST['wpgas_group_pickup_nonce'])) : '';
-    if (!$nonce || !wp_verify_nonce($nonce, 'wpgas_save_group_pickup')) {
+function wgo_save_group_pickup_field($group_id) {
+    $nonce = isset($_POST['wgo_group_pickup_nonce']) ? sanitize_text_field(wp_unslash($_POST['wgo_group_pickup_nonce'])) : '';
+    if (!$nonce || !wp_verify_nonce($nonce, 'wgo_save_group_pickup')) {
         return;
     }
 
-    if (isset($_POST['wpgas_group_pickup'])) {
-        $pickup = sanitize_text_field(wp_unslash($_POST['wpgas_group_pickup']));
-        groups_update_groupmeta($group_id, 'wpgas_group_pickup', $pickup);
+    if (isset($_POST['wgo_group_pickup'])) {
+        $pickup = sanitize_text_field(wp_unslash($_POST['wgo_group_pickup']));
+        groups_update_groupmeta($group_id, 'wgo_group_pickup', $pickup);
     }
 }
-add_action('groups_custom_group_fields_updated', 'wpgas_save_group_pickup_field');
+add_action('groups_custom_group_fields_updated', 'wgo_save_group_pickup_field');
