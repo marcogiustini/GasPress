4cebfbbe0a7f18c7075efa1f34ebcbfce3df7b6f
Marco Giustini <info@marcogiustini.info>
2025-08-28 11:36:56 +0200

    Update and rename notifications.php to wgo-notifications.php

--- includes/wgo-notifications.php
+++ includes/wgo-notifications.php
@@ -1,3 +1,4 @@
+<?php
 function wgo_notify_user($user_id, $message, $action = 'group_order') {
     bp_notifications_add_notification([
         'user_id' => $user_id,
@@ -7,5 +8,6 @@ function wgo_notify_user($user_id, $message, $action = 'group_order') {
         'date_notified' => bp_core_current_time(),
         'is_new' => 1,
     ]);
+
     wp_mail(get_userdata($user_id)->user_email, 'Notifica Gruppo', $message);
 }
