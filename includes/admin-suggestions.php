6f92ef908ebcb49635999ad890bc3f40adc9650d
Marco Giustini <info@marcogiustini.info>
2025-08-28 11:38:10 +0200

    Update and rename admin-suggestions.php to wgo-admin-suggestions.php

--- includes/admin-suggestions.php
+++
@@ -1,3 +0,0 @@
-function wgo_render_suggestions_admin($group_id) {
-    // Visualizza suggerimenti, approva/ignora, aggiunge alla vetrina
-}
---
+++ includes/wgo-admin-suggestions.php
@@ -0,0 +1,7 @@
+<?php
+function wgo_render_suggestions_admin($group_id) {
+    echo '<div class="wgo-suggestions-admin">';
+    echo '<h4>Suggerimenti prodotti dal gruppo</h4>';
+    echo '<p>Funzionalità in sviluppo: approva, ignora o aggiungi alla vetrina.</p>';
+    echo '</div>';
+}
