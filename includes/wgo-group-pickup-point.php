d9b4680dec98d0f0e4d5d1da0382f912acabd6d7
Marco Giustini <info@marcogiustini.info>
2025-08-28 11:35:35 +0200

    Update and rename group-pickup-point.php to wgo-group-pickup-point.php

--- includes/wgo-group-pickup-point.php
+++ includes/wgo-group-pickup-point.php
@@ -1,3 +1,4 @@
+<?php
 function wgo_set_group_pickup_point($group_id, $address) {
     update_group_meta($group_id, 'wgo_group_pickup_point', sanitize_text_field($address));
 }
@@ -12,15 +13,15 @@ function wgo_render_group_pickup_form() {
     $group_id = bp_get_group_id();
     $current = wgo_get_group_pickup_point($group_id);
 
-    echo '<h4>Punto di Ritiro del Gruppo</h4>';
     echo '<form method="post">';
-    echo '<input type="text" name="wgo_group_pickup" value="' . esc_attr($current) . '" style="width: 400px;">';
-    echo '<p><input type="submit" name="wgo_save_group_pickup" class="button button-primary" value="Salva"></p>';
+    echo '<label for="wgo_group_pickup">Punto di Ritiro del Gruppo</label><br>';
+    echo '<input type="text" name="wgo_group_pickup" value="' . esc_attr($current) . '" />';
+    echo '<input type="submit" name="wgo_save_group_pickup" value="Salva" />';
     echo '</form>';
 
     if (isset($_POST['wgo_save_group_pickup'])) {
         wgo_set_group_pickup_point($group_id, $_POST['wgo_group_pickup']);
-        echo '<div class="updated"><p>Punto di ritiro del gruppo aggiornato.</p></div>';
+        echo '<p>Punto di ritiro aggiornato.</p>';
     }
 }
 add_action('bp_after_group_home_content', 'wgo_render_group_pickup_form');
