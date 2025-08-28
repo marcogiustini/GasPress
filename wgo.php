92d0bd1854b85479c6865fedde7593fed404ba50
Marco Giustini <info@marcogiustini.info>
2025-08-28 17:33:16 +0200

    Update wgo.php

--- wgo.php
+++ wgo.php
@@ -1,11 +1,11 @@
 <?php
 /*
-Plugin Name: GasPress
+Plugin Name: GasPress – Gruppi di acquisto solidali
 Description: Gestione ordini per gruppi di acquisto solidali con BuddyPress, WooCommerce, Wallet e Dokan.
 Version: 1.0.0
 Author: Marco Giustini
 Author URI: https://github.com/marcogiustini
-Text Domain: gaspress-main
+Text Domain: GasPress-main
 Domain Path: /languages
 Requires at least: 6.0
 Tested up to: 6.8
@@ -24,13 +24,13 @@ $plugin_data = get_file_data(__FILE__, ['Version' => 'Version']);
 $plugin_version = $plugin_data['Version'];
 
 add_action('wp_enqueue_scripts', function () use ($plugin_version) {
-    wp_enqueue_style('wgo-style', plugin_dir_url(__FILE__) . 'assets/css/wgo-style.css', [], $plugin_version);
-    wp_enqueue_script('wgo-script', plugin_dir_url(__FILE__) . 'assets/js/wgo-script.js', [], $plugin_version, true);
+    wp_enqueue_style('gaspress-style', plugin_dir_url(__FILE__) . 'assets/css/gaspress-style.css', [], $plugin_version);
+    wp_enqueue_script('gaspress-script', plugin_dir_url(__FILE__) . 'assets/js/gaspress-script.js', [], $plugin_version, true);
 });
 
 add_action('admin_init', function () {
     if (!function_exists('wc_get_product') || !function_exists('groups_get_group_members')) {
         deactivate_plugins(plugin_basename(__FILE__));
-        wp_die(esc_html__('Questo plugin richiede WooCommerce e BuddyPress attivi.', 'gaspress-main'));
+        wp_die(esc_html__('Questo plugin richiede WooCommerce e BuddyPress attivi.', 'GasPress-main'));
     }
 });
