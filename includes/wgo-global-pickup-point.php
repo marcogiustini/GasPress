ab83a864beeaaf3dd7d5b1153a04e4ffc11955d4
Marco Giustini <info@marcogiustini.info>
2025-08-28 11:36:16 +0200

    Update and rename global-pickup-point.php to wgo-global-pickup-point.php

--- includes/wgo-global-pickup-point.php
+++ includes/wgo-global-pickup-point.php
@@ -1,3 +1,4 @@
+<?php
 function wgo_set_global_pickup_point($address) {
     update_option('wgo_global_pickup_point', sanitize_text_field($address));
 }
@@ -11,14 +12,16 @@ function wgo_render_global_pickup_settings() {
 
     echo '<div class="wrap"><h2>Punto di Ritiro Generale</h2>';
     echo '<form method="post">';
-    echo '<input type="text" name="wgo_global_pickup" value="' . esc_attr(wgo_get_global_pickup_point()) . '" style="width: 400px;">';
-    echo '<p><input type="submit" name="wgo_save_global_pickup" class="button button-primary" value="Salva"></p>';
-    echo '</form></div>';
+    echo '<input type="text" name="wgo_global_pickup" value="' . esc_attr(wgo_get_global_pickup_point()) . '" />';
+    echo '<input type="submit" name="wgo_save_global_pickup" value="Salva" />';
+    echo '</form>';
 
     if (isset($_POST['wgo_save_global_pickup'])) {
         wgo_set_global_pickup_point($_POST['wgo_global_pickup']);
-        echo '<div class="updated"><p>Punto di ritiro aggiornato.</p></div>';
+        echo '<p>Punto di ritiro aggiornato.</p>';
     }
+
+    echo '</div>';
 }
 add_action('admin_menu', function () {
     add_submenu_page('woocommerce', 'Punto di Ritiro', 'Punto di Ritiro', 'manage_woocommerce', 'wgo-pickup-settings', 'wgo_render_global_pickup_settings');
