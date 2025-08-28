e84872aefd674ab39e3838214c6919ada7cc04c0
Marco Giustini <info@marcogiustini.info>
2025-08-28 16:27:11 +0200

    Update wgo-functions-shared.php

--- includes/wgo-functions-shared.php
+++ includes/wgo-functions-shared.php
@@ -1,27 +1,36 @@
 <?php
-function wgo_group_items_by_vendor($wc_order) {
-    $vendor_map = [];
-    foreach ($wc_order->get_items() as $item) {
-        $product = $item->get_product();
-        if (!$product) continue;
-        $vendor_id = get_post_field('post_author', $product->get_id());
-        $vendor_map[$vendor_id][] = [
-            'name' => $product->get_name(),
-            'qty' => $item->get_quantity(),
-            'price' => $item->get_total(),
-        ];
-    }
-    return $vendor_map;
-}
+/**
+ * Funzioni condivise per WP GAS
+ */
 
-function wgo_get_active_order_for_group($group_id) {
-    $orders = get_posts([
-        'post_type' => 'group_order',
-        'meta_query' => [[
-            'key' => 'group_id',
-            'value' => $group_id,
-            'compare' => '='
-        ]]
-    ]);
-    return !empty($orders) ? $orders[0] : false;
+defined('ABSPATH') || exit;
+
+/**
+ * Restituisce i prodotti assegnati a un gruppo BuddyPress
+ *
+ * @param int $group_id
+ * @return array
+ */
+function wgo_get_group_products($group_id) {
+    if (!is_numeric($group_id) || $group_id <= 0) {
+        return [];
+    }
+
+    $args = [
+        'post_type'      => 'product',
+        'post_status'    => 'publish',
+        'posts_per_page' => -1,
+        'fields'         => 'ids',
+        'meta_query'     => [
+            [
+                'key'     => 'wgo_group_id',
+                'value'   => intval($group_id),
+                'compare' => '=',
+                'type'    => 'NUMERIC',
+            ],
+        ],
+    ];
+
+    $query = new WP_Query($args);
+    return $query->posts;
 }
